344595c3f5960438f018627a1e61265368666891
Jen Heilemann <jheilema@nerdery.com>
2013-06-18 14:03:07 -0400

    Add a full title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  # returns the page title on a per-page basis
+  #
+  # param string The title of the page to add to the base title
+  def full_title(page_title)
+    base_title = "Sample App"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
