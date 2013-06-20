d493868838b062623bff4229398d200d5115a2e1
Jen Heilemann <jheilema@nerdery.com>
2013-06-20 16:09:20 -0400

    Speed up the BCrypt functionality for the test environment... because who cares about password security in the test environment.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,4 +34,11 @@ SampleApp::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  # Speed up tests by lowering BCrypt's cost function
+  # (we don't care about password security in the test suite)
+  require 'bcrypt'
+  silence_warnings do
+    BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
+  end
 end
