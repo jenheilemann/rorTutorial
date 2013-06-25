28641f5e01ce0eff4ff6bba6bb0fe11c6a7357d3
Jen Heilemann <jheilema@nerdery.com>
2013-06-25 15:54:51 -0400

    Implements user sign out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,5 +17,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    # sign the user out
+    sign_out
+    redirect_to root_url
   end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -48,6 +48,17 @@ describe "Authentication" do
       it { should     have_link('Profile', href: user_path(user)) }
       it { should     have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
+
+      describe "move around the site" do
+        before { click_link "Contact" }
+        it { should have_link "Sign out" }
+        it { should_not have_link('Sign in', href: signin_path) }
+      end
+
+      describe "followed with signout" do
+        before { click_link "Sign out" }
+        it { should have_link "Sign in" }
+      end
     end
   end
 end
