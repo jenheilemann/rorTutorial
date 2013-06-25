ff6c01b14a0f02350ed7541b9b487e39d3e81242
Jen Heilemann <jheilema@nerdery.com>
2013-06-25 15:42:00 -0400

    Creating the ability to sign in.... yay

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  include SessionsHelper
+
+  # force signout to prevent cross-site request forgery attacks
+  def handle_unverified_request
+    sign_out
+
+    super
+  end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -5,8 +5,49 @@ describe "Authentication" do
 
   describe "signin page" do
     before { visit signin_path }
+    let(:signin) { "Sign in" }
 
     it { should have_selector('h1',    text: "Sign in") }
     it { should have_selector('title', text: "Sign in") }
+
+    describe "with invalid information" do
+      before do
+        fill_in "Email",    with: "fakeuser"
+        fill_in "Password", with: "fakepassword"
+        click_button signin
+      end
+
+      it { should have_selector('title', text: "Sign in") }
+      it { should have_selector('div.alert.alert-error', text: "Invalid") }
+
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
+      end
+    end
+
+    describe "with blank information" do
+      before do
+        click_button signin
+      end
+
+      it { should have_selector('title', text: "Sign in") }
+      it { should have_selector('div.alert.alert-error', text: "Invalid") }
+    end
+
+    describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        fill_in "Email",    with: user.email.upcase
+        fill_in "Password", with: user.password
+        click_button signin
+      end
+
+      it { current_path.should == user_path(user) }
+      it { should have_selector('title', text: user.name) }
+      it { should     have_link('Profile', href: user_path(user)) }
+      it { should     have_link('Sign out', href: signout_path) }
+      it { should_not have_link('Sign in', href: signin_path) }
+    end
   end
 end
